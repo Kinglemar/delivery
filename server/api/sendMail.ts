8a76b34e2928f832a82a81f7680b76801a8451b5
kinglemar <dimeji.ogunleye20@gmail.com>
2023-06-20 14:55:04 +0100

    Logger.

--- server/api/sendMail.ts
+++ server/api/sendMail.ts
@@ -1,9 +1,11 @@
 import { Resend } from 'resend';
 import logger from './middleware/logger';
 const resend = new Resend(process.env.EMAIL_KEY);
+console.log(process.env.EMAIL_KEY)
 const useEnvEmail = process.env.EMAIL_ADDRESS
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
+    logger(body)
     logger(body)
     try {
         const data = await resend.emails.send({
